1e7611d47baefdabb99b056cd3399635b2ad3365
mpds <matheusmullerps@gmail.com>
2023-10-03 17:29:42 -0300

    fix out of bound values in initial population

--- algorithms/genetic/population.py
+++ algorithms/genetic/population.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 import numpy as np
 
 __all__ = ["Population"]
@@ -8,11 +6,28 @@ __all__ = ["Population"]
 class Population:
     """Initializes a population with values from a gaussian distribution."""
 
-    def __init__(self, population_size: int, n_vars: int = 32, seed: int = 42) -> None:
+    def __init__(
+        self,
+        population_size: int,
+        n_vars: int = 32,
+        seed: int = 42,
+        xl: float = -2.5,
+        xu: float = 2.5,
+    ) -> None:
         self.popsize = population_size
         self.n_vars = n_vars
+        self.xl = xl
+        self.xu = xu
         self.rng = np.random.default_rng(seed)
+
+    def _clip(self, population: np.ndarray) -> np.ndarray:
+        """Clips the population values to the upper and lower bounds."""
+        population[population > self.xu] = self.xu - np.random.random()
+        population[population < self.xl] = self.xl + np.random.random()
+        return population
 
     def initialize(self) -> None:
         """Each gene value is sampled from a gaussian distribution."""
-        return self.rng.normal(size=(self.popsize, self.n_vars))
+        population = self.rng.normal(size=(self.popsize, self.n_vars))
+        population = self._clip(population)
+        return population
