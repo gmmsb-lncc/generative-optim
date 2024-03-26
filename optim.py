cf9709d65020a61afae15486778e09a99c22affe
mpds <matheusmullerps@gmail.com>
2024-03-26 17:59:34 -0300

    Update objectives configuration file

--- optim.py
+++ optim.py
@@ -1,4 +1,6 @@
 import argparse
+import importlib
+import json
 import logging
 import os
 import subprocess
@@ -9,14 +11,14 @@ from aim import Run
 from pymoo.core.algorithm import Algorithm
 from pymoo.optimize import minimize
 
+import problems
 from algorithms import AlgorithmFactory
 from algorithms.callbacks import AimCallback
 from algorithms.operators import (BinaryTournament, GaussianMutation_,
                                   PointCrossover)
 from algorithms.population import Population
 from hgraph.hiervae import HierVAEDecoder
-from objectives_conf import define_objectives
-from problems import ProblemFactory
+from problems.molecular_problem import ProblemFactory
 
 
 def main(args: argparse.Namespace):
@@ -46,7 +48,7 @@ def main(args: argparse.Namespace):
         termination=termination_criteria,
         seed=args.seed,
         verbose=args.verbose,
-        callback=AimCallback(run, n_obj=len(args.optim_probs)),
+        callback=AimCallback(run, n_obj=problem.n_obj),
     )
 
     # export final population
@@ -56,6 +58,9 @@ def main(args: argparse.Namespace):
         f"generated_molecules_run={run.hash}.txt",
         HierVAEDecoder(),
     )
+    # track final population
+    with open(f"generated_molecules_run={run.hash}.txt", "r") as f:
+        run.track(aim.Text(f.read()), name="final_population")
 
 
 def configure_callback(args: argparse.Namespace, algorithm: Algorithm) -> Run:
@@ -64,21 +69,42 @@ def configure_callback(args: argparse.Namespace, algorithm: Algorithm) -> Run:
     args.git_hash = _get_git_revision_hash()
     run["hparams"] = vars(args)
     run.track(_get_this_file(), name="script")
-    run.track(_get_objectives_conf_file(), name="objectives_conf")
+    run.track(_get_objectives_conf_file(args), name="objs_conf")
     return run
 
 
 def configure_problem(args: argparse.Namespace):
-    problems = define_objectives()
-    problem_factory = ProblemFactory()
-    for k, v in problems.items():
-        problem_factory.register_problem(k, v.problem)
 
-    print("Objs:", [(p, problems[p].target_value) for p in args.optim_probs])
+    avail_probs = {
+        p: getattr(problems, p)
+        for p in problems.__all__
+    }
+
+    def determine_type(target):
+        try:
+            return float(target)
+        except ValueError:
+            return str(target)
+
+    with open(args.objs_file, "r") as f:
+        objs = json.load(f)
+
+    user_problems = {}
+    for obj in objs["objectives"]:  # name, target
+        if obj["name"] not in avail_probs:
+            raise ValueError(f"Objective '{obj['name']}' not found in available objectives: {avail_probs.keys()}")
+        
+        user_problems[f"{obj['name']}_{obj['target']}"] = (avail_probs[obj["name"]], determine_type(obj["target"]))
+
+    problem_factory = ProblemFactory()
+    for k, v in user_problems.items():
+        problem_factory.register_problem(k, v[0])
+
+    print("Objectives: ", user_problems)
 
     problem = problem_factory.create_problem(
-        problem_identifiers=args.optim_probs,
-        targets=[problems[p].target_value for p in args.optim_probs],
+        problem_identifiers=list(user_problems.keys()),
+        targets=[v[1] for v in user_problems.values()],    
         n_var=args.num_vars,
         lbound=args.lbound,
         ubound=args.ubound,
@@ -99,8 +125,8 @@ def _get_this_file() -> None:
     return file
 
 
-def _get_objectives_conf_file() -> None:
-    with open("objectives_conf.py", "r") as f:
+def _get_objectives_conf_file(args) -> None:
+    with open(args.objs_file, "r") as f:
         file = aim.Text(f.read())
     return file
 
@@ -121,12 +147,12 @@ if __name__ == "__main__":
 
     # problem parameters
     problem_args = parser.add_argument_group("problem arguments")
-    problem_args.add_argument("--optim-probs", type=str, nargs="+", default=["MW"], help=f"optimization problem; options: {', '.join(define_objectives().keys())}")
+    problem_args.add_argument("--objs-file", type=str, default="objectives.conf.json", help=f"path to objectives configuration file; objectives must be configure inside the conf file. available options: {', '.join(problems.__all__)}")
     problem_args.add_argument("--num-vars", type=int, default=32, help="number of variables")
 
     # genetic algorithm parameters
     ga_args = parser.add_argument_group("genetic algorithm arguments")
-    ga_args.add_argument("--algorithm", type=str, default="GA", help=f"algorithm type; options: {', '.join(AlgorithmFactory.algorithm_map.keys())}")
+    ga_args.add_argument("--algorithm", type=str, default="GA", help=f"algorithm type; available options: {', '.join(AlgorithmFactory.algorithm_map.keys())}")
     ga_args.add_argument("--population-size", type=int, default=40, help="population size")
     ga_args.add_argument("--num-offspring", type=int, default=None, help="number of offspring")
     ga_args.add_argument("--mutation-sigma", type=float, default=0.01, help="gaussian mutation strength")
